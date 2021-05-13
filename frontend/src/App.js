15a874b72b068dd9cd879ef6f3a183d172b88ad4
Yuki Kubota <rakudalms@gmail.com>
2021-05-13 11:50:20 +0900

    URL get /restaurants/xxx/foods

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,15 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  withRouter,
 } from 'react-router-dom';
 
 // components
-import { Restaurants } from './containers/Restaurants.jsx'
-import { Foods } from './containers/Foods.jsx'
-import { Orders } from './containers/Orders.jsx'
+import { Restaurants } from './containers/Restaurants.jsx';
+import { Foods } from './containers/Foods.jsx';
+import { Orders } from './containers/Orders.jsx';
 
 function App() {
   return (
@@ -34,6 +33,16 @@ function App() {
          path="/orders">
            <Orders />
         </Route>
+
+        <Route
+        exact
+        path="/restaurants/:restaurantsId/foods"
+        render={({ match }) =>
+          <Foods
+          match={match}
+          />
+        }
+        />
       </Switch>
     </Router>
   )
